d57253a47177f81ed65a6db082d13e564ea18f05
Rogerio Prado de Jesus <rogeriopradoj@gmail.com>
2024-05-12 03:45:29 -0300

    Fix #16 - Make git-fflow-finish works with main and master branches

--- bin/git-fflow-finish.bat
+++ bin/git-fflow-finish.bat
@@ -1,9 +1,19 @@
 @echo off
 setlocal EnableDelayedExpansion
 
+set "main_branch=main"
+
+git show-ref --heads !main_branch! > nul 2>&1
+if not %errorlevel% == "0" (
+    set "main_branch=master"
+)
+
 for /f "tokens=*" %%a in ('git symbolic-ref --short HEAD') do set "BRANCH=%%a"
 
-git checkout master 
+echo Branch: !BRANCH!
+echo main_branch: !main_branch!
+
+git checkout !main_branch! 
 git merge !BRANCH! --no-ff --no-edit 
 git branch -d !BRANCH!
 
